c9bbf174d7dcd6f72bd092e768f0783ace224031
bbrodriges <bender.rodriges@gmail.com>
2021-09-01 21:42:49 +0300

    change json test check

--- cmd/shortenertest/http_handlers_test.go
+++ cmd/shortenertest/http_handlers_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/json"
 	"errors"
 	"math/big"
 	mathrand "math/rand"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
@@ -68,48 +70,46 @@ func TestAPIHandler(t *testing.T) {
 	endpointURL := config.TargetAddress + "/api/shorten"
 	targetURL := generateTestURL(t)
 
-	// create HTTP client without redirects support
-	errRedirectBlocked := errors.New("HTTP redirect blocked")
-	httpc := resty.New().
-		SetCloseConnection(true).
-		SetRedirectPolicy(
-			resty.RedirectPolicyFunc(func(_ *http.Request, _ []*http.Request) error {
-				return errRedirectBlocked
-			}),
-		)
-
-	resp, err := httpc.R().
-		SetHeader("Content-Type", "application/json").
-		SetBody(&shortenRequest{
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).
+		Encode(&shortenRequest{
 			URL: targetURL,
-		}).
-		Post(endpointURL)
-	if !assert.NoError(t, err) {
-		return
-	}
+		})
+
+	require.NoError(t, err, "cannot marshal request body")
+
+	req := httptest.NewRequest("POST", endpointURL, &buf)
+	req.Header.Set("Content-Type", "application/json")
+	req.RequestURI = ""
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
 
 	var result shortenResponse
-	err = json.Unmarshal(resp.Body(), &result)
+	err = json.NewDecoder(resp.Body).Decode(&result)
 	if !assert.NoError(t, err) {
 		return
 	}
 
 	shortenURL := result.Result
 
-	assert.Equal(t, http.StatusCreated, resp.StatusCode())
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
 	assert.NoError(t, func() error {
 		_, err := url.Parse(shortenURL)
 		return err
 	}())
 
 	// expand URL
-	resp, err = httpc.R().Get(shortenURL)
-	if !errors.Is(err, errRedirectBlocked) && !assert.NoError(t, err) {
-		return
-	}
+	req = httptest.NewRequest("GET", shortenURL, bytes.NewBuffer(nil))
+	req.RequestURI = ""
 
-	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode())
-	assert.Equal(t, targetURL, resp.Header().Get("Location"))
+	resp, err = http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	assert.Equal(t, http.StatusTemporaryRedirect, resp.StatusCode)
+	assert.Equal(t, targetURL, resp.Header.Get("Location"))
 }
 
 func generateTestURL(t *testing.T) string {
