d0c01e744fccb3973ded3935176fa5e96866be4e
Alexander Demidenko <hurd@yandex-team.ru>
2021-09-23 21:47:55 +0700

    tests for increments 6

--- cmd/devopstest/iteration6_test.go
+++ cmd/devopstest/iteration6_test.go
@@ -17,61 +17,62 @@ import (
 	"github.com/Yandex-Practicum/go-autotests/internal/fork"
 )
 
-// Iteration4Suite is a suite of autotests
-type Iteration4Suite struct {
+// Iteration6Suite is a suite of autotests
+type Iteration6Suite struct {
 	suite.Suite
 
 	serverAddress string
+	serverPort    string
 	serverProcess *fork.BackgroundProcess
 	agentProcess  *fork.BackgroundProcess
+	// knownPgLibraries []string
 
-	knownEncodingLibs []string
-
-	rnd *rand.Rand
+	rnd  *rand.Rand
+	envs []string
 }
 
-type Metrics struct {
-	ID    string   `json:"id"`
-	MType string   `json:"type"`            // Параметр кодирую строкой, принося производительность в угоду наглядности.
-	Delta *int64   `json:"delta,omitempty"` //counter
-	Value *float64 `json:"value,omitempty"` //gauge
-}
-
-// SetupSuite bootstraps suite dependencies
-func (suite *Iteration4Suite) SetupSuite() {
+func (suite *Iteration6Suite) SetupSuite() {
 	// check required flags
 	suite.Require().NotEmpty(flagTargetSourcePath, "-source-path non-empty flag required")
 	suite.Require().NotEmpty(flagServerBinaryPath, "-binary-path non-empty flag required")
 	suite.Require().NotEmpty(flagAgentBinaryPath, "-agent-binary-path non-empty flag required")
+	suite.Require().NotEmpty(flagServerPort, "-server-port non-empty flag required")
+	suite.Require().NotEmpty(flagFileStoragePath, "-file-storage-path non-empty flag required")
 
 	suite.rnd = rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	// suite.knownPgLibraries = []string{
+	// 	"database/sql",
+	// 	"github.com/jackc/pgx",
+	// 	"github.com/lib/pq",
+	// }
+	suite.serverAddress = "http://localhost:" + flagServerPort
+	suite.serverPort = flagServerPort
 
-	suite.knownEncodingLibs = []string{
-		"encoding/json",
-		"github.com/mailru/easyjson",
-		"github.com/pquerna/ffjson",
-	}
+	suite.envs = append(os.Environ(), []string{
+		"ADDRESS=localhost:" + flagServerPort,
+		"REPORT_INTERVAL=10s",
+		"POLL_INTERVAL=2s",
 
-	suite.serverAddress = "http://localhost:8080"
+		"SHUTDOWN_TIMEOUT=5s",
+		"RESTORE=true",
+		"STORE_INTERVAL=5m",
+		"STORE_FILE=" + flagFileStoragePath,
+	}...)
 
-	// start server
-	{
-		envs := append(os.Environ(), "DATABASE_DSN="+flagDatabaseDSN)
-		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-		defer cancel()
-		suite.agentUp(ctx, envs, "8080")
-		suite.serverUp(ctx, envs, "8080")
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	suite.agentUp(ctx, suite.envs, flagServerPort)
+	suite.serverUp(ctx, suite.envs, flagServerPort)
 }
 
-func (suite *Iteration4Suite) serverUp(ctx context.Context, envs []string, port string) {
+func (suite *Iteration6Suite) serverUp(ctx context.Context, envs []string, port string) {
 	p := fork.NewBackgroundProcess(context.Background(), flagServerBinaryPath,
 		fork.WithEnv(envs...),
 	)
 
 	err := p.Start(ctx)
 	if err != nil {
-		suite.T().Errorf("Невозможно запустить процесс командой %s: %s. Переменные окружения: %+v", p, err, envs)
+		suite.T().Errorf("Невозможно запустить процесс командой %q: %s. Переменные окружения: %+v", p, err, envs)
 		return
 	}
 
@@ -83,7 +84,7 @@ func (suite *Iteration4Suite) serverUp(ctx context.Context, envs []string, port 
 	suite.serverProcess = p
 }
 
-func (suite *Iteration4Suite) agentUp(ctx context.Context, envs []string, port string) {
+func (suite *Iteration6Suite) agentUp(ctx context.Context, envs []string, port string) {
 	p := fork.NewBackgroundProcess(context.Background(), flagAgentBinaryPath,
 		fork.WithEnv(envs...),
 	)
@@ -103,12 +104,12 @@ func (suite *Iteration4Suite) agentUp(ctx context.Context, envs []string, port s
 }
 
 // TearDownSuite teardowns suite dependencies
-func (suite *Iteration4Suite) TearDownSuite() {
+func (suite *Iteration6Suite) TearDownSuite() {
 	suite.agentShutdown()
 	suite.serverShutdown()
 }
 
-func (suite *Iteration4Suite) serverShutdown() {
+func (suite *Iteration6Suite) serverShutdown() {
 	if suite.serverProcess == nil {
 		return
 	}
@@ -140,7 +141,7 @@ func (suite *Iteration4Suite) serverShutdown() {
 	}
 }
 
-func (suite *Iteration4Suite) agentShutdown() {
+func (suite *Iteration6Suite) agentShutdown() {
 	if suite.agentProcess == nil {
 		return
 	}
@@ -172,19 +173,7 @@ func (suite *Iteration4Suite) agentShutdown() {
 	}
 }
 
-// TestEncoderUsage attempts to recursively find usage of known HTTP frameworks in given sources
-func (suite *Iteration4Suite) TestEncoderUsage() {
-	err := usesKnownPackage(suite.T(), flagTargetSourcePath, suite.knownEncodingLibs)
-	if errors.Is(err, errUsageNotFound) {
-		suite.T().Errorf("Не найдено использование известных библиотек кодирования JSON %q", flagTargetSourcePath)
-		return
-	}
-	if !errors.Is(err, errUsageFound) {
-		suite.T().Errorf("Неожиданная ошибка при поиске использования библиотек кодирования JSON по пути %q: %v", flagTargetSourcePath, err)
-	}
-}
-
-func (suite *Iteration4Suite) TestCounterHandlers() {
+func (suite *Iteration6Suite) TestCounterHandlers() {
 	// create HTTP client without redirects support
 	errRedirectBlocked := errors.New("HTTP redirect blocked")
 	redirPolicy := resty.RedirectPolicyFunc(func(_ *http.Request, _ []*http.Request) error {
@@ -195,6 +184,7 @@ func (suite *Iteration4Suite) TestCounterHandlers() {
 		SetRedirectPolicy(redirPolicy)
 
 	id := strconv.Itoa(suite.rnd.Intn(256))
+	var storage int64
 
 	suite.Run("update", func() {
 		value1, value2 := suite.rnd.Int63(), suite.rnd.Int63()
@@ -256,6 +246,7 @@ func (suite *Iteration4Suite) TestCounterHandlers() {
 
 		dumpErr = dumpErr && suite.Assert().Containsf(resp.Header().Get("Content-Type"), "application/json",
 			"Заголовок ответа Content-Type содержит несоответствующее значение")
+
 		dumpErr = dumpErr && suite.NotNil(result.Delta,
 			"Несоответствие отправленного значения counter (%d) полученному от сервера (nil), '%q %s'", value0+value1+value2, req.Method, req.URL)
 
@@ -268,10 +259,58 @@ func (suite *Iteration4Suite) TestCounterHandlers() {
 			dump = dumpResponse(resp.RawResponse, true)
 			suite.T().Logf("Оригинальный ответ:\n\n%s", dump)
 		}
+
+		storage = value0 + value1 + value2
 	})
+
+	suite.Run("restart server", func() {
+		time.Sleep(5 * time.Second) // relax time
+		suite.serverShutdown()
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+		suite.serverUp(ctx, suite.envs, suite.serverPort)
+	})
+
+	suite.Run("get", func() {
+		req := httpc.R().
+			SetHeader("Content-Type", "application/json")
+
+		// Вдруг на сервере уже есть значение, на всякий случай запросим.
+		var result Metrics
+		resp, err := req.SetBody(&Metrics{
+			ID:    "GetSet" + id,
+			MType: "counter"}).
+			SetResult(&result).
+			Post("value/")
+
+		dumpErr := suite.Assert().NoError(err, "Ошибка при попытке сделать запрос с получением значения counter")
+
+		dumpErr = dumpErr && suite.Assert().Equalf(http.StatusOK, resp.StatusCode(),
+			"Несоответствие статус кода ответа ожидаемому в хендлере %q: %q ", req.Method, req.URL)
+		dumpErr = dumpErr && suite.Assert().Equalf(http.StatusOK, resp.StatusCode(),
+			"Несоответствие статус кода ответа ожидаемому в хендлере %q: %q ", req.Method, req.URL)
+		dumpErr = dumpErr && suite.Assert().Containsf(resp.Header().Get("Content-Type"), "application/json",
+			"Заголовок ответа Content-Type содержит несоответствующее значение")
+		dumpErr = dumpErr && suite.NotNil(result.Delta,
+			"Получено не инициализированное значение Delta '%q %s'", req.Method, req.URL)
+
+		dumpErr = dumpErr && suite.NotNil(result.Delta,
+			"Несоответствие ожидаемого значения counter (%d) полученному от сервера (nil), '%q %s'", storage, req.Method, req.URL)
+
+		dumpErr = dumpErr && suite.Assert().Equalf(storage, *result.Delta,
+			"Несоответствие ожидаемого значения counter (%d) полученному от сервера (%d), '%q %s'", storage, *result.Delta, req.Method, req.URL)
+
+		if !dumpErr {
+			dump := dumpRequest(req.RawRequest, true)
+			suite.T().Logf("Оригинальный запрос:\n\n%s", dump)
+			dump = dumpResponse(resp.RawResponse, true)
+			suite.T().Logf("Оригинальный ответ:\n\n%s", dump)
+		}
+	})
+
 }
 
-func (suite *Iteration4Suite) TestGaugeHandlers() {
+func (suite *Iteration6Suite) TestGaugeHandlers() {
 	errRedirectBlocked := errors.New("HTTP redirect blocked")
 	redirPolicy := resty.RedirectPolicyFunc(func(_ *http.Request, _ []*http.Request) error {
 		return errRedirectBlocked
@@ -281,6 +320,7 @@ func (suite *Iteration4Suite) TestGaugeHandlers() {
 		SetRedirectPolicy(redirPolicy)
 
 	id := strconv.Itoa(suite.rnd.Intn(256))
+	var storage float64
 
 	suite.Run("update", func() {
 		value := suite.rnd.Float64() * 1e6
@@ -306,13 +346,10 @@ func (suite *Iteration4Suite) TestGaugeHandlers() {
 		dumpErr = dumpErr && suite.Assert().NoError(err, "Ошибка при попытке сделать запрос с получением значения gauge")
 		dumpErr = dumpErr && suite.Assert().Equalf(http.StatusOK, resp.StatusCode(),
 			"Несоответствие статус кода ответа ожидаемому в хендлере %q: %q ", req.Method, req.URL)
-
 		dumpErr = dumpErr && suite.Assert().Containsf(resp.Header().Get("Content-Type"), "application/json",
 			"Заголовок ответа Content-Type содержит несоответствующее значение")
-
 		dumpErr = dumpErr && suite.Assert().NotEqualf(nil, result.Value,
 			"Несоответствие отправленного значения gauge (%f) полученному от сервера (nil), '%q %s'", value, req.Method, req.URL)
-
 		dumpErr = dumpErr && suite.Assert().Equalf(value, *result.Value,
 			"Несоответствие отправленного значения gauge (%f) полученному от сервера (%f), '%q %s'", value, *result.Value, req.Method, req.URL)
 
@@ -322,144 +359,44 @@ func (suite *Iteration4Suite) TestGaugeHandlers() {
 			dump = dumpResponse(resp.RawResponse, true)
 			suite.T().Logf("Оригинальный ответ:\n\n%s", dump)
 		}
+		storage = *result.Value
 	})
-}
 
-func (suite *Iteration4Suite) TestCollectAgentMetrics() {
-	errRedirectBlocked := errors.New("HTTP redirect blocked")
-	redirPolicy := resty.RedirectPolicyFunc(func(_ *http.Request, _ []*http.Request) error {
-		return errRedirectBlocked
+	suite.Run("restart server", func() {
+		time.Sleep(5 * time.Second) // relax time
+		suite.serverShutdown()
+		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+		defer cancel()
+		suite.serverUp(ctx, suite.envs, suite.serverPort)
 	})
-	httpc := resty.New().
-		SetHostURL(suite.serverAddress).
-		SetRedirectPolicy(redirPolicy)
 
-	tests := []struct {
-		name   string
-		method string
-		value  float64
-		delta  int64
-		update int
-		ok     bool
-		static bool
-	}{
-		{method: "counter", name: "PollCount"},
-		{method: "gauge", name: "RandomValue"},
-		{method: "gauge", name: "Alloc"},
-		{method: "gauge", name: "BuckHashSys", static: true},
-		{method: "gauge", name: "Frees"},
-		{method: "gauge", name: "GCCPUFraction", static: true},
-		{method: "gauge", name: "GCSys", static: true},
-		{method: "gauge", name: "HeapAlloc"},
-		{method: "gauge", name: "HeapIdle"},
-		{method: "gauge", name: "HeapInuse"},
-		{method: "gauge", name: "HeapObjects"},
-		{method: "gauge", name: "HeapReleased", static: true},
-		{method: "gauge", name: "HeapSys", static: true},
-		{method: "gauge", name: "LastGC", static: true},
-		{method: "gauge", name: "Lookups", static: true},
-		{method: "gauge", name: "MCacheInuse", static: true},
-		{method: "gauge", name: "MCacheSys", static: true},
-		{method: "gauge", name: "MSpanInuse", static: true},
-		{method: "gauge", name: "MSpanSys", static: true},
-		{method: "gauge", name: "Mallocs"},
-		{method: "gauge", name: "NextGC", static: true},
-		{method: "gauge", name: "NumForcedGC", static: true},
-		{method: "gauge", name: "NumGC", static: true},
-		{method: "gauge", name: "OtherSys", static: true},
-		{method: "gauge", name: "PauseTotalNs", static: true},
-		{method: "gauge", name: "StackInuse", static: true},
-		{method: "gauge", name: "StackSys", static: true},
-		{method: "gauge", name: "Sys", static: true},
-		{method: "gauge", name: "TotalAlloc"},
-	}
+	suite.Run("get", func() {
+		req := httpc.R().
+			SetHeader("Content-Type", "application/json")
 
-	req := httpc.R().
-		SetHeader("Content-Type", "application/json")
+		// Вдруг на сервере уже есть значение, на всякий случай запросим.
+		var result Metrics
+		resp, err := req.SetBody(&Metrics{
+			ID:    "GetSet" + id,
+			MType: "gauge"}).
+			SetResult(&result).
+			Post("value/")
 
-	timer := time.NewTimer(time.Minute)
+		dumpErr := suite.Assert().NoError(err, "Ошибка при попытке сделать запрос с получением значения gauge")
+		dumpErr = dumpErr && suite.Assert().Equalf(http.StatusOK, resp.StatusCode(),
+			"Несоответствие статус кода ответа ожидаемому в хендлере %q: %q ", req.Method, req.URL)
+		dumpErr = dumpErr && suite.Assert().Containsf(resp.Header().Get("Content-Type"), "application/json",
+			"Заголовок ответа Content-Type содержит несоответствующее значение")
+		dumpErr = dumpErr && suite.Assert().NotEqualf(nil, result.Value,
+			"Несоответствие ожидаемого значения gauge (%f) полученному от сервера (nil), '%q %s'", storage, req.Method, req.URL)
+		dumpErr = dumpErr && suite.Assert().Equalf(storage, *result.Value,
+			"Несоответствие ожидаемого значения gauge (%f) полученному от сервера (%f), '%q %s'", storage, *result.Value, req.Method, req.URL)
 
-cont:
-	for ok := 0; ok != len(tests); {
-		// suite.T().Log("tick", len(tests)-ok)
-		select {
-		case <-timer.C:
-			break cont
-		default:
+		if !dumpErr {
+			dump := dumpRequest(req.RawRequest, true)
+			suite.T().Logf("Оригинальный запрос:\n\n%s", dump)
+			dump = dumpResponse(resp.RawResponse, true)
+			suite.T().Logf("Оригинальный ответ:\n\n%s", dump)
 		}
-		for i, tt := range tests {
-			if tt.ok {
-				continue
-			}
-			var (
-				resp *resty.Response
-				err  error
-			)
-			time.Sleep(100 * time.Millisecond)
-
-			var result Metrics
-			resp, err = req.SetBody(&Metrics{
-				ID:    tt.name,
-				MType: tt.method,
-			}).
-				SetResult(&result).
-				Post("/value/")
-
-			dumpErr := suite.Assert().NoErrorf(err, "Ошибка при попытке сделать запрос с получением значения %s", tt.name)
-
-			if resp.StatusCode() == http.StatusNotFound {
-				continue
-			}
-
-			dumpErr = dumpErr && suite.Assert().Containsf(resp.Header().Get("Content-Type"), "application/json",
-				"Заголовок ответа Content-Type содержит несоответствующее значение")
-
-			dumpErr = dumpErr && suite.Assert().True(((result.MType == "gauge" && result.Value != nil) || (result.MType == "counter" && result.Delta != nil)),
-				"Получен не однозначный результат (тип метода не соответствует возвращаемому значению) '%q %s'", req.Method, req.URL)
-
-			dumpErr = dumpErr && suite.Assert().False(result.Delta == nil && result.Value == nil,
-				"Получен результат без данных (Dalta == nil && Value == nil) '%q %s'", req.Method, req.URL)
-
-			dumpErr = dumpErr && suite.Assert().False(result.Delta != nil && result.Value != nil,
-				"Получен не однозначный результат (Dalta != nil && Value != nil) '%q %s'", req.Method, req.URL)
-
-			dumpErr = dumpErr && suite.Assert().Equalf(http.StatusOK, resp.StatusCode(),
-				"Несоответствие статус кода ответа ожидаемому в хендлере %q: %q ", req.Method, req.URL)
-
-			dumpErr = dumpErr && suite.Assert().True(result.MType == "gauge" || result.MType == "counter",
-				"Получен ответ с неизвестным значением типа: %q, '%q %s'", result.MType, req.Method, req.URL)
-
-			if !dumpErr {
-				dump := dumpRequest(req.RawRequest, true)
-				suite.T().Logf("Оригинальный запрос:\n\n%s", dump)
-				dump = dumpResponse(resp.RawResponse, true)
-				suite.T().Logf("Оригинальный ответ:\n\n%s", dump)
-				return
-			}
-
-			switch tt.method {
-			case "gauge":
-				if (tt.update != 0 && *result.Value != tt.value) || tt.static {
-					tests[i].ok = true
-					ok++
-					suite.T().Logf("get %s: %q, value: %f", tt.method, tt.name, *result.Value)
-				}
-				tests[i].value = *result.Value
-			case "counter":
-				if (tt.update != 0 && *result.Delta != tt.delta) || tt.static {
-					tests[i].ok = true
-					ok++
-					suite.T().Logf("get %s: %q, value: %d", tt.method, tt.name, *result.Delta)
-				}
-				tests[i].delta = *result.Delta
-			}
-
-			tests[i].update++
-		}
-	}
-	for _, tt := range tests {
-		suite.Run(tt.method+"/"+tt.name, func() {
-			suite.Assert().Truef(tt.ok, "Отсутствует изменение метрики: %s, тип: %s", tt.name, tt.method)
-		})
-	}
+	})
 }
