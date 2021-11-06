65f9b8901a41b7f5850ea2fd07aef20aa779489a
bbrodriges <bender.rodriges@gmail.com>
2021-11-06 10:14:06 +0300

    fix luhn generator

--- cmd/gophermarttest/helpers.go
+++ cmd/gophermarttest/helpers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -40,29 +39,38 @@ func generateOrderNumber(t *testing.T) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("cannot calculate check digit: %s", err)
 	}
-	return ds + strconv.FormatUint(uint64(cd), 10), nil
+	return ds + strconv.Itoa(cd), nil
 }
 
-func luhnCheckDigit(s string) (uint8, error) {
-	if s == "" {
-		return 0, errors.New("empty string given")
+func luhnCheckDigit(s string) (int, error) {
+	number, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
 	}
 
-	var sum uint
-	var alter bool
-	for i := len(s) - 1; i >= 0; i-- {
-		if s[i] < '0' || s[i] > '9' {
-			return 0, errors.New("non-numeric character found")
-		}
+	checkNumber := luhnChecksum(number)
 
-		d := uint(s[i] - '0')
-		if alter {
-			d *= 2
-		}
-		sum += d / 10
-		sum += d % 10
-		alter = !alter
+	if checkNumber == 0 {
+		return 0, nil
 	}
+	return 10 - checkNumber, nil
+}
 
-	return uint8(sum % 10), nil
+func luhnChecksum(number int) int {
+	var luhn int
+
+	for i := 0; number > 0; i++ {
+		cur := number % 10
+
+		if i%2 == 0 { // even
+			cur = cur * 2
+			if cur > 9 {
+				cur = cur%10 + cur/10
+			}
+		}
+
+		luhn += cur
+		number = number / 10
+	}
+	return luhn % 10
 }
