7a5ca4cf1c63119d5fdc84001e9764c8d7d91454
bbrodriges <bender.rodriges@gmail.com>
2023-05-12 12:01:12 +0300

    refactor usesKnownPackage helper

--- cmd/metricstest/iteration3a_test.go
+++ cmd/metricstest/iteration3a_test.go
@@ -9,8 +9,8 @@ import (
 type Iteration3ASuite struct {
 	suite.Suite
 
-	knownFrameworks      []string
-	restrictedFrameworks []string
+	knownFrameworks      PackageRules
+	restrictedFrameworks PackageRules
 }
 
 // SetupSuite подготавливает необходимые зависимости
@@ -19,97 +19,97 @@ func (suite *Iteration3ASuite) SetupSuite() {
 	suite.Require().NotEmpty(flagTargetSourcePath, "-source-path non-empty flag required")
 
 	// список известных фреймворков
-	suite.knownFrameworks = []string{
-		"aahframework.org",
-		"confetti-framework.com",
-		"github.com/abahmed/gearbox",
-		"github.com/aerogo/aero",
-		"github.com/aisk/vox",
-		"github.com/ant0ine/go-json-rest",
-		"github.com/aofei/air",
-		"github.com/appist/appy",
-		"github.com/astaxie/beego",
-		"github.com/beatlabs/patron",
-		"github.com/bnkamalesh/webgo",
-		"github.com/claygod/Bxog",
-		"github.com/claygod/microservice",
-		"github.com/dimfeld/httptreemux",
-		"github.com/dinever/golf",
-		"github.com/fulldump/golax",
-		"github.com/gernest/alien",
-		"github.com/gernest/utron",
-		"github.com/gin-gonic/gin",
-		"github.com/go-chi/chi",
-		"github.com/go-goyave/goyave",
-		"github.com/go-macaron/macaron",
-		"github.com/go-ozzo/ozzo-routing",
-		"github.com/go-playground/lars",
-		"github.com/go-playground/pure",
-		"github.com/go-zoo/bone",
-		"github.com/goa-go/goa",
-		"github.com/goadesign/goa",
-		"github.com/goanywhere/rex",
-		"github.com/gocraft/web",
-		"github.com/gofiber/fiber",
-		"github.com/goji/goji",
-		"github.com/gookit/rux",
-		"github.com/gorilla/mux",
-		"github.com/goroute/route",
-		"github.com/gotuna/gotuna",
-		"github.com/gowww/router",
-		"github.com/GuilhermeCaruso/bellt",
-		"github.com/hidevopsio/hiboot",
-		"github.com/husobee/vestigo",
-		"github.com/i-love-flamingo/flamingo",
-		"github.com/i-love-flamingo/flamingo-commerce",
-		"github.com/ivpusic/neo",
-		"github.com/julienschmidt/httprouter",
-		"github.com/labstack/echo",
-		"github.com/lunny/tango",
-		"github.com/mustafaakin/gongular",
-		"github.com/nbari/violetear",
-		"github.com/nsheremet/banjo",
-		"github.com/NYTimes/gizmo",
-		"github.com/paulbellamy/mango",
-		"github.com/rainycape/gondola",
-		"github.com/razonyang/fastrouter",
-		"github.com/rcrowley/go-tigertonic",
-		"github.com/resoursea/api",
-		"github.com/revel/revel",
-		"github.com/rs/xmux",
-		"github.com/twharmon/goweb",
-		"github.com/uadmin/uadmin",
-		"github.com/ungerik/go-rest",
-		"github.com/vardius/gorouter",
-		"github.com/VividCortex/siesta",
-		"github.com/xujiajun/gorouter",
-		"github.com/xxjwxc/ginrpc",
-		"github.com/yarf-framework/yarf",
-		"github.com/zpatrick/fireball",
-		"gobuffalo.io",
-		"rest-layer.io",
+	suite.knownFrameworks = PackageRules{
+		{Name: "aahframework.org"},
+		{Name: "confetti-framework.com"},
+		{Name: "github.com/abahmed/gearbox"},
+		{Name: "github.com/aerogo/aero"},
+		{Name: "github.com/aisk/vox"},
+		{Name: "github.com/ant0ine/go-json-rest"},
+		{Name: "github.com/aofei/air"},
+		{Name: "github.com/appist/appy"},
+		{Name: "github.com/astaxie/beego"},
+		{Name: "github.com/beatlabs/patron"},
+		{Name: "github.com/bnkamalesh/webgo"},
+		{Name: "github.com/claygod/Bxog"},
+		{Name: "github.com/claygod/microservice"},
+		{Name: "github.com/dimfeld/httptreemux"},
+		{Name: "github.com/dinever/golf"},
+		{Name: "github.com/fulldump/golax"},
+		{Name: "github.com/gernest/alien"},
+		{Name: "github.com/gernest/utron"},
+		{Name: "github.com/gin-gonic/gin"},
+		{Name: "github.com/go-chi/chi"},
+		{Name: "github.com/go-goyave/goyave"},
+		{Name: "github.com/go-macaron/macaron"},
+		{Name: "github.com/go-ozzo/ozzo-routing"},
+		{Name: "github.com/go-playground/lars"},
+		{Name: "github.com/go-playground/pure"},
+		{Name: "github.com/go-zoo/bone"},
+		{Name: "github.com/goa-go/goa"},
+		{Name: "github.com/goadesign/goa"},
+		{Name: "github.com/goanywhere/rex"},
+		{Name: "github.com/gocraft/web"},
+		{Name: "github.com/gofiber/fiber"},
+		{Name: "github.com/goji/goji"},
+		{Name: "github.com/gookit/rux"},
+		{Name: "github.com/gorilla/mux"},
+		{Name: "github.com/goroute/route"},
+		{Name: "github.com/gotuna/gotuna"},
+		{Name: "github.com/gowww/router"},
+		{Name: "github.com/GuilhermeCaruso/bellt"},
+		{Name: "github.com/hidevopsio/hiboot"},
+		{Name: "github.com/husobee/vestigo"},
+		{Name: "github.com/i-love-flamingo/flamingo"},
+		{Name: "github.com/i-love-flamingo/flamingo-commerce"},
+		{Name: "github.com/ivpusic/neo"},
+		{Name: "github.com/julienschmidt/httprouter"},
+		{Name: "github.com/labstack/echo"},
+		{Name: "github.com/lunny/tango"},
+		{Name: "github.com/mustafaakin/gongular"},
+		{Name: "github.com/nbari/violetear"},
+		{Name: "github.com/nsheremet/banjo"},
+		{Name: "github.com/NYTimes/gizmo"},
+		{Name: "github.com/paulbellamy/mango"},
+		{Name: "github.com/rainycape/gondola"},
+		{Name: "github.com/razonyang/fastrouter"},
+		{Name: "github.com/rcrowley/go-tigertonic"},
+		{Name: "github.com/resoursea/api"},
+		{Name: "github.com/revel/revel"},
+		{Name: "github.com/rs/xmux"},
+		{Name: "github.com/twharmon/goweb"},
+		{Name: "github.com/uadmin/uadmin"},
+		{Name: "github.com/ungerik/go-rest"},
+		{Name: "github.com/vardius/gorouter"},
+		{Name: "github.com/VividCortex/siesta"},
+		{Name: "github.com/xujiajun/gorouter"},
+		{Name: "github.com/xxjwxc/ginrpc"},
+		{Name: "github.com/yarf-framework/yarf"},
+		{Name: "github.com/zpatrick/fireball"},
+		{Name: "gobuffalo.io"},
+		{Name: "rest-layer.io"},
 	}
 
 	// список запрещенных фреймворков
-	suite.restrictedFrameworks = []string{
-		"github.com/valyala/fasthttp",
-		"github.com/fasthttp/router",
+	suite.restrictedFrameworks = PackageRules{
+		{Name: "github.com/valyala/fasthttp"},
+		{Name: "github.com/fasthttp/router"},
 	}
 }
 
 // TestFrameworkUsage пробует рекурсивно найти хотя бы одно использование известных фреймворков в директории с исходным кодом проекта
 func (suite *Iteration3ASuite) TestFrameworkUsage() {
 	// проверяем наличие запрещенных фреймворков
-	err := usesKnownPackage(suite.T(), flagTargetSourcePath, suite.restrictedFrameworks)
-	if err == nil {
+	err := usesKnownPackage(suite.T(), flagTargetSourcePath, suite.restrictedFrameworks...)
+	if errors.Is(err, errUsageFound) {
 		suite.T().Errorf("Найдено использование одного из не рекомендуемых фреймворков по пути %s: %s",
-			flagTargetSourcePath, suite.restrictedFrameworks)
+			flagTargetSourcePath, suite.restrictedFrameworks.PackageList())
 		return
 	}
 
 	// проверяем наличие известных фреймворков
-	err = usesKnownPackage(suite.T(), flagTargetSourcePath, suite.knownFrameworks)
-	if err == nil {
+	err = usesKnownPackage(suite.T(), flagTargetSourcePath, suite.knownFrameworks...)
+	if errors.Is(err, errUsageFound) {
 		return
 	}
 	if errors.Is(err, errUsageNotFound) {
