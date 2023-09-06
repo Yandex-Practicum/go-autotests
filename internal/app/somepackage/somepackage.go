bc2dc17e702b561e626fc1fe4277f27bcd88aa0d
Rockython <61116928+Rockython@users.noreply.github.com>
2023-09-06 16:15:05 +0300

    inc1 done

---
+++ internal/app/somepackage/somepackage.go
@@ -0,0 +1,7 @@
+package somepackage
+
+import "fmt"
+
+func PrintHello() {
+	fmt.Println("Hello")
+}
---
+++ internal/services/shurlService/shurlService.go
@@ -0,0 +1,20 @@
+package shurlService
+
+import (
+	"math/rand"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func EvaluateHashAndReturn() string {
+	hashCode := RandStringRunes(5)
+	return hashCode
+}
