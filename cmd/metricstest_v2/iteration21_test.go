6ad458c054b4789508182100186a37cb8cdc0aa3
Владислав Попов <lekan.lekan@gmail.com>
2025-09-30 22:01:18 +0300

    tests for iteration 21

---
+++ cmd/metricstest_v2/iteration21_test.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"math/rand"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/Yandex-Practicum/go-autotests/internal/fork"
+	"github.com/stretchr/testify/suite"
+)
+
+type Iteration21Suite struct {
+	suite.Suite
+
+	serverAddress string
+	serverPort    string
+	serverProcess *fork.BackgroundProcess
+	agentProcess  *fork.BackgroundProcess
+
+	rnd *rand.Rand
+
+	key []byte
+}
+
+func (suite *Iteration21Suite) SetupSuite() {
+	suite.Require().NotEmpty(flagTargetSourcePath, "-source-path non-empty flag required")
+	suite.Require().NotEmpty(flagServerBinaryPath, "-binary-path non-empty flag required")
+	suite.Require().NotEmpty(flagAgentBinaryPath, "-agent-binary-path non-empty flag required")
+	suite.Require().NotEmpty(flagServerPort, "-server-port non-empty flag required")
+	suite.Require().NotEmpty(flagSHA256Key, "-key non-empty flag required")
+	suite.Require().NotEmpty(flagDatabaseDSN, "-database-dsn non-empty flag required")
+
+	suite.rnd = rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
+	suite.serverAddress = "http://localhost:" + flagServerPort
+	suite.serverPort = flagServerPort
+
+	suite.key = []byte(flagSHA256Key)
+
+	envs := append(os.Environ(), []string{
+		"ADDRESS=localhost:" + flagServerPort,
+		"RESTORE=true",
+		"DATABASE_DSN=" + flagDatabaseDSN,
+		"KEY=" + flagSHA256Key,
+	}...)
+
+	agentArgs := []string{
+		"-k=" + flagSHA256Key,
+	}
+	serverArgs := []string{
+		"-k=invalidkey",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	suite.agentUp(ctx, envs, agentArgs, flagServerPort)
+	suite.serverUp(ctx, envs, serverArgs, flagServerPort)
+}
+
+func (suite *Iteration21Suite) serverUp(ctx context.Context, envs, args []string, port string) {
+	suite.serverProcess = fork.NewBackgroundProcess(context.Background(), flagServerBinaryPath,
+		fork.WithEnv(envs...),
+		fork.WithArgs(args...),
+	)
+
+	err := suite.serverProcess.Start(ctx)
+	if err != nil {
+		suite.T().Errorf("Невозможно запустить процесс командой %q: %s. Переменные окружения: %+v, флаги командной строки: %+v", suite.serverProcess, err, envs, args)
+		return
+	}
+
+	err = suite.serverProcess.WaitPort(ctx, "tcp", port)
+	if err != nil {
+		suite.T().Errorf("Не удалось дождаться пока порт %s станет доступен для запроса: %s", port, err)
+		return
+	}
+}
+
+func (suite *Iteration21Suite) agentUp(ctx context.Context, envs, args []string, port string) {
+	suite.agentProcess = fork.NewBackgroundProcess(context.Background(), flagAgentBinaryPath,
+		fork.WithEnv(envs...),
+		fork.WithArgs(args...),
+	)
+
+	err := suite.agentProcess.Start(ctx)
+	if err != nil {
+		suite.T().Errorf("Невозможно запустить процесс командой %q: %s. Переменные окружения: %+v, флаги командной строки: %+v", suite.agentProcess, err, envs, args)
+		return
+	}
+
+	err = suite.agentProcess.ListenPort(ctx, "tcp", port)
+	if err != nil {
+		suite.T().Errorf("Не удалось дождаться пока на порт %s начнут поступать данные: %s", port, err)
+		return
+	}
+}
+
+func (suite *Iteration21Suite) TearDownSuite() {
+	suite.agentShutdown()
+	suite.serverShutdown()
+}
+
+func (suite *Iteration21Suite) serverShutdown() {
+	if suite.serverProcess == nil {
+		return
+	}
+
+	exitCode, err := suite.serverProcess.Stop(syscall.SIGINT, syscall.SIGKILL)
+	if err != nil {
+		if errors.Is(err, os.ErrProcessDone) {
+			return
+		}
+		suite.T().Logf("Не удалось остановить процесс с помощью сигнала ОС: %s", err)
+		return
+	}
+
+	if exitCode > 0 {
+		suite.T().Logf("Процесс завершился с не нулевым статусом %d", exitCode)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	out := suite.serverProcess.Stderr(ctx)
+	if len(out) > 0 {
+		suite.T().Logf("Получен STDERR лог процесса:\n\n%s", string(out))
+	}
+	out = suite.serverProcess.Stdout(ctx)
+	if len(out) > 0 {
+		suite.T().Logf("Получен STDOUT лог процесса:\n\n%s", string(out))
+	}
+}
+
+func (suite *Iteration21Suite) agentShutdown() {
+	if suite.agentProcess == nil {
+		return
+	}
+
+	exitCode, err := suite.agentProcess.Stop(syscall.SIGINT, syscall.SIGKILL)
+	if err != nil {
+		if errors.Is(err, os.ErrProcessDone) {
+			return
+		}
+		suite.T().Logf("Не удалось остановить процесс с помощью сигнала ОС: %s", err)
+		return
+	}
+
+	if exitCode > 0 {
+		suite.T().Logf("Процесс завершился с не нулевым статусом %d", exitCode)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	out := suite.agentProcess.Stderr(ctx)
+	if len(out) > 0 {
+		suite.T().Logf("Получен STDERR лог процесса:\n\n%s", string(out))
+	}
+	out = suite.agentProcess.Stdout(ctx)
+	if len(out) > 0 {
+		suite.T().Logf("Получен STDOUT лог процесса:\n\n%s", string(out))
+	}
+}
